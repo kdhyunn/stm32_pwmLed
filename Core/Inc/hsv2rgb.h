059733dcd1683ef787b9fdd668b80866f377e985
kdhyunn <4802rnehdgus@naver.com>
2024-01-11 16:10:54 +0900

    this is my first project

---
+++ Core/Inc/hsv2rgb.h
@@ -0,0 +1,27 @@
+/*
+ * hsv2rgb.h
+ *
+ *  Created on: Jan 11, 2024
+ *      Author: gdh13
+ */
+
+#ifndef _HSV2RGB_H
+#define _HSV2RGB_H
+
+/* hsv2rgb.h */
+
+struct hsv_colour {
+   float h;
+   float s;
+   float v;
+}; /* end struct */
+
+struct rgb_colour {
+   float r;
+   float g;
+   float b;
+}; /* end struct */
+
+int hsv2rgb( struct hsv_colour *hsv, struct rgb_colour *rgb );
+
+#endif
